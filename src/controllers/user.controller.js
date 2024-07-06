84ead9b4497ed58412387d2fa951989841594c2f
Bharat Kumar <bharatk503@gmail.com>
2024-07-06 18:58:23 +0530

    register user completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,69 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { APIError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+
 
 export const registerUser = asyncHandler ( async (req,res)=>{
-    res.status(200).json({
-        message: "ok"
+    // get user info from client
+    const {fullName,email,username,password}=req.body;
+    // console.table([fullName,username,email,password]);
+    if(fullName===""){
+        throw new APIError(400,"full name is required")
+    }
+
+  const existedUser= await User.findOne({
+        $or:[{username},{email}]
     })
+    if(existedUser){
+        throw new APIError(409,"user already existing")
+    }
+    const avatarLocalPath= req.files?.avatar[0]?.path;
+    const coverImageLocalPath= req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new APIError(400,"Avatar file is required");
+    }
+
+    const avatar=await uploadOnCloudinary(avatarLocalPath);
+    const coverImage= await uploadOnCloudinary(coverImageLocalPath);
+    //check if user already exist(validation)
+    if(avatar){
+        throw new APIError(400,"Avatar upload failed");
+    }
+
+    const user=await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+    
+    const createduser= await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if(!createduser){
+        throw new APIError(500,"something went wrong while registering the user");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createduser,"user register successfully")
+    )
+
+    //check for images and avtar
+
+    //upload them to clodineryNam
+
+    //create user object-- create entry in db
+
+    //remove password and refresh token field
+
+    //check for user creation
+
+
 })
 
 export const loginUser = async (req,res)=>{
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,18 +1,25 @@
 import { Router } from "express";
-import { getUser, loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  getUser,
+  loginUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 // router.post('/register',)
 // router.route("/register").post(registerUser)
-router.post('/register',registerUser)
-router.post('/login',loginUser)
+router.post(
+  "/register",
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
+router.post("/login", loginUser);
 
-router.get('/user',getUser);
-
-
-
-
-
+router.get("/user", getUser);
 
 export default router;
